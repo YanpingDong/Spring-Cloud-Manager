95e82cd317d4180f861db6c6cc341b3b66fe1e37
YanpingDong <dyp@tcl.com>
2017-01-15 22:58:58 +0800

    uniform flask run config style

--- backend/deploy-manage-service.py
+++ backend/deploy-manage-service.py
@@ -3,5 +3,7 @@ from app import app
 from config import GlobalVar
 
 if __name__ == '__main__':
-    app.debug = GlobalVar.PROPERTIES.get('debug')           # 设置调试模式，生产模式的时候要关掉debug
-    app.run(host=GlobalVar.PROPERTIES.get('host'))                  # 启动服务器
+    #app.debug = GlobalVar.PROPERTIES.get('debug')           # 设置调试模式，生产模式的时候要关掉debug
+    app.run(host=GlobalVar.PROPERTIES.get('host'),
+            port=int(GlobalVar.PROPERTIES.get('port')),
+            debug=GlobalVar.PROPERTIES.get('debug'))                  # 启动服务器
