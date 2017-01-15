b3495cf39fe694191a41b72471d9d58b3d3acc83
YanpingDong <dyp@tcl.com>
2017-01-15 22:37:47 +0800

    add properties file parser

--- backend/deploy-manage-service.py
+++ backend/deploy-manage-service.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
-from app import app          
+from app.utils.Properties import Properties
+from app import app
+from config import GlobalVar
 
 if __name__ == '__main__':
-    app.debug = True           # 设置调试模式，生产模式的时候要关掉debug
-    app.run(host='localhost')                  # 启动服务器
+    properties = Properties(GlobalVar.BASE_DIR + '/config.properties')
+    app.debug = properties.get('debug')           # 设置调试模式，生产模式的时候要关掉debug
+    app.run(host=properties.get('host'))                  # 启动服务器
