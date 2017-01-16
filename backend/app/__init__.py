e856915c7da1fd01bdeb7d7dfcd10cc9b69464fa
YanpingDong <lear521@163.com>
2017-01-16 10:31:44 +0800

    don't trace *.pyc file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask        # 引入 flask
 from flask import redirect
 from flask import render_template
 from flask import url_for
-from flask.ext.cors import CORS
+from flask_cors import CORS
 from config import GlobalVar
 
 app = Flask(__name__, template_folder=GlobalVar.BASE_DIR + '/templates')          # 实例化一个flask 对象
